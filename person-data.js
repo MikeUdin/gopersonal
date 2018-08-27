398d3f7f48602bc69034403dc89008b47626b227
Mike <bondura2@gmail.com>
2018-08-27 19:51:51 +0200

    New branch added

--- person-data.js
+++ person-data.js
@@ -1,7 +1,6 @@
 var data ={"num": "3", 
            "name": "Trixy",
             "direction": "NY"}
-var somedata = {"name":"Make",
-                "lastname":"Udin",
-                "address":"Street Downhill",
-                }
+var somedata = {"name":"Make", "lastname":"Udin", "address":"Street Downhill", }
+var somedata = {"name":"Make", "lastname":"Udin", "address":"Street Downhill", }
+var somedata = {"name":"Make", "lastname":"Udin", "address":"Street Downhill", }
